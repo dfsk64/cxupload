62e89df1b5ec5cf5b00eaea82d05fa5948f6f2ec
wangfeng <dfsk64@outlook.com>
2018-03-03 14:58:50 +0800

    cxupload demo

---
+++ cxupload.js
@@ -0,0 +1,24 @@
+$.extend({
+    cxupload: function (conf) {
+        $('#' + conf.fileId).on('change', function () {
+            var fileList = $(this)[0].files;
+            var fd = new FormData();
+            for (var i = 0; i < fileList.length; i++) {
+                fd.append(conf.file, fileList[i]);
+            }
+            $.ajax({
+                type: "post",
+                url: conf.url,
+                data: fd,
+                processData: false,
+                contentType: false,
+                success: function (data) {
+                    conf.success(data);
+                },
+                error: function (data) {
+                    conf.error(data);
+                }
+            });
+        });
+    }
+})
